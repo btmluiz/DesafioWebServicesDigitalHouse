d47a7bffb279bbff9e4505e6640a6634a7297c7e
Luiz Felipe Silva Braga <btmluiz@outlook.com>
2020-12-04 23:52:19 -0300

    Remoção de bad imports

--- app/src/main/java/dev/luizfelipe/desafiowebservices_digitalhouse/ui/main/DescriptionFragment.kt
+++ app/src/main/java/dev/luizfelipe/desafiowebservices_digitalhouse/ui/main/DescriptionFragment.kt
@@ -3,7 +3,6 @@ package dev.luizfelipe.desafiowebservices_digitalhouse.ui.main
 import android.content.Context
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,13 +11,10 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
-import androidx.navigation.fragment.FragmentNavigatorExtras
-import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import dev.luizfelipe.desafiowebservices_digitalhouse.R
 import dev.luizfelipe.desafiowebservices_digitalhouse.models.Result
 import java.text.SimpleDateFormat
-import java.util.*
 
 class DescriptionFragment : DialogFragment() {
     private lateinit var comic: Result
